1e7e248d52105269160bd565116bfde4390060ce
Finbar Berkon <finbar.berkon@unsw.edu.au>
2020-06-16 23:06:18 +1000

    add tut02 skeleton code

---
+++ tut02/leap_year.c
@@ -0,0 +1,27 @@
+//
+//
+//
+
+#include <stdio.h>
+
+int main(void) {
+    int year;
+    printf("Please enter a year: ");
+    scanf("%d", &year);
+
+    if (/* our first condition goes here */) {
+        if (/* our second condition goes here */) {
+            if (/* our third condition goes here */) {
+
+            } else {
+
+            }
+        } else {
+
+        }
+    } else {
+
+    }
+
+    return 0;
+}
